f08a5e4a6a205cae385bfff3b5ddf99d38d8533b
william-emel <wemel3618@student.pps.net>
2024-03-12 11:38:11 -0700

    gamemanager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,6 +11,16 @@ public class GameManager : MonoBehaviour
     public List<Card> player_hand = new List<Card>();
     public List<Card> ai_hand = new List<Card>();
     public List<Card> discard_pile = new List<Card>();
+    public int playerscore;
+    public int aiscore;
+    public int target;
+    public int playerdif;
+    public int aidif;
+    public int playerpoints;
+    public int aipoints;
+    public bool playerout;
+    public bool aiout;
+    public Card selectedcard;
 
     private void Awake()
     {
@@ -27,28 +37,70 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerout = false;
+        aiout = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (deck.Count <= 5)
+        {
+            Shuffle();
+        }
+
+        if (playerscore > target)
+        {
+            Debug.Log("you went over, you lose");
+            playerout = true;
+            round_end();
+        }
         
+      // check player and ai score against target score
     }
 
     void Deal()
     {
-
+        // give each player two cards, hiding 
     }
 
     void Shuffle()
     {
-
+        // return all cards to the deck 
     }
 
     void AI_Turn()
     {
+        // draw card
+        if (target - aiscore <= 11)
+        {
+            player_hand.Add(selectedcard);
+        }
+    }
 
+    void round_end()
+    {
+        playerdif = target - playerscore;
+        aidif = target - aiscore;
+        if (playerdif < aidif && playerdif >= 0)
+        {
+            Debug.Log("player wins");
+            playerpoints += 1;
+            // discard all the cards from both players hands
+        } else if (aidif < playerdif && aidif < 0) 
+        {
+            Debug.Log("AI wins");
+            aipoints += 1;
+            // discard all the cards from both players hands
+        } else if (aidif == playerdif)
+        {
+            Debug.Log("tie, no one gets any points");
+           // discard all the cards from bother players hands
+        }
+        else
+        {
+            Debug.Log("This shouldn't have came up, something is wrong.");
+        }
     }
 
 
